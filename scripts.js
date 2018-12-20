9c7271bb5579175acfc15e5a57e9ffb293b36f60
Shani Fedida <shani.fedida@gmail.com>
2018-12-20 11:28:42 +0200

    refactor - change addStock to not handle day data

--- scripts.js
+++ scripts.js
@@ -1,44 +1,43 @@
 "use strict";
 function addStock() {
-    const ul = document.getElementById("stock_list");   
+    const ul = document.getElementById("stock_list");
     var li = document.createElement("li");
-    
+
     const dayLbl = document.createElement("label");
-    let day = ul.children.length+1;   
-    dayLbl.innerText=`Day ${day}:`;
-    dayLbl.setAttribute("class","DayLabel");
+    dayLbl.setAttribute("class", "DayLabel");
     li.appendChild(dayLbl);
-    
+
     const input = document.createElement("input");
     li.appendChild(input);
-    
+
     const deleteBtn = document.createElement("button");
-    deleteBtn.innerText="Delete";
-    deleteBtn.setAttribute("type","button");
-    deleteBtn.setAttribute("day",day);
+    deleteBtn.innerText = "Delete";
+    deleteBtn.setAttribute("type", "button");
     deleteBtn.onclick = deleteStock;
-    deleteBtn.setAttribute("class","DeleteButton");
+    deleteBtn.setAttribute("class", "DeleteButton");
     li.appendChild(deleteBtn);
-                  
-    ul.appendChild(li);               
-  }
 
-  function deleteStock(e){
-    const deleteBtn = e.target; 
+    ul.appendChild(li);
+
+    fillDayData();
+}
+
+function deleteStock(e) {
+    const deleteBtn = e.target;
     let day = deleteBtn.getAttribute("day");
-    const ul = document.getElementById("stock_list");   
-    ul.removeChild(ul.children[day-1]);
-    rearrange();
+    const ul = document.getElementById("stock_list");
+    ul.removeChild(ul.children[day - 1]);
+    fillDayData();
+}
 
-    function rearrange(){
-        const ul = document.getElementById("stock_list");   
-        for( let day =1; day <= ul.childElementCount; ++day){
-            const li = ul.children[day-1];
-            const dayLabel = li.getElementsByClassName("DayLabel")[0];
-            dayLabel.innerText=`Day ${day}:`;
+function fillDayData() {
+    const ul = document.getElementById("stock_list");
+    for (let day = 1; day <= ul.childElementCount; ++day) {
+        const li = ul.children[day - 1];
+        const dayLabel = li.getElementsByClassName("DayLabel")[0];
+        dayLabel.innerText = `Day ${day}:`;
 
-            const deleteBtn = li.getElementsByClassName("DeleteButton")[0];
-            deleteBtn.setAttribute("day",day);
-        }    
+        const deleteBtn = li.getElementsByClassName("DeleteButton")[0];
+        deleteBtn.setAttribute("day", day);
     }
-  }
+}
