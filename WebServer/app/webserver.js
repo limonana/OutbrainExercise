ee3ddcc15d46d19f14d8f90483191fada8e54220
Shani Fedida <shani.fedida@gmail.com>
2018-12-20 18:36:00 +0200

    move api to webserver

--- WebServer/app/webserver.js
+++ WebServer/app/webserver.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var router = express.Router();
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+router.post('/Decision', function(req, res) {
+    const stockValues = req.body.StockValues;
+    let decision = findBestDecision(stockValues);    
+    res.json(decision);   
+});
 
 app.use(express.static(__dirname))
 
@@ -12,3 +27,16 @@ app.get('/', function(req, res) {
 let port= 8080;
 app.listen(port);
 console.log('Webserver Listen on port ' + port);
+
+function findBestDecision(stockValues){
+    let maxDecision = {buyingDay : 0, sellingDay: 0, revenue:0 }
+    for ( buyingDay =1 ; buyingDay<=stockValues.length; ++buyingDay){
+        for ( sellingDay = buyingDay+1; sellingDay <= stockValues.length; ++sellingDay){
+            let revenue = stockValues[sellingDay-1] - stockValues[buyingDay-1];
+            if (revenue > maxDecision.revenue){
+                maxDecision = {buyingDay: buyingDay, sellingDay: sellingDay, revenue: revenue};
+            }
+        }
+    }
+    return maxDecision;
+}
