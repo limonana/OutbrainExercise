a314a589d76dc362d4d089e012d2dace79085ab7
Shani Fedida <shani.fedida@gmail.com>
2018-12-20 12:06:39 +0200

    refactor list element to function

--- scripts.js
+++ scripts.js
@@ -1,10 +1,13 @@
 "use strict";
+function getListElement(){
+    return document.getElementById("stock_list");
+}
 
 function addStock() {
     var temp = document.getElementsByTagName("template")[0];
     var clon = temp.content.cloneNode(true);
     
-    const ul = document.getElementById("stock_list");
+    const ul = getListElement();
     ul.appendChild(clon);
 
     fillDayData();
@@ -12,14 +15,14 @@ function addStock() {
 
 function deleteStock(deleteBtn) {
     let day = deleteBtn.getAttribute("day");
-    const ul = document.getElementById("stock_list");
+    const ul = getListElement();
     ul.removeChild(ul.children[day - 1]);
     
     fillDayData();
 }
 
 function fillDayData() {
-    const ul = document.getElementById("stock_list");
+    const ul = getListElement();
     for (let day = 1; day <= ul.childElementCount; ++day) {
         const li = ul.children[day - 1];
         const dayLabel = li.getElementsByClassName("DayLabel")[0];
