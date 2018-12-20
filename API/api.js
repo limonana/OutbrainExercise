abdb2b0e4199d37b9b83d12776f47297f127d490
Shani Fedida <shani.fedida@gmail.com>
2018-12-20 14:02:46 +0200

    create basic api

---
+++ API/api.js
@@ -0,0 +1,30 @@
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+var router = express.Router();              // get an instance of the express Router
+
+router.post('/Decision', function(req, res) {
+    res.json({
+        "BuyDay": 1,
+        "SellDay" : 2,
+        "Revenue": 10
+    });   
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
